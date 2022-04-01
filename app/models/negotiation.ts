9596f984b82013bcbb72df20128c13aa848e6829
Guilherme César <ssguicesar@gmail.com>
2022-04-01 09:53:24 -0300

    chore: simplifying code

--- app/models/negotiation.ts
+++ app/models/negotiation.ts
@@ -1,27 +1,53 @@
 export class Negotiation {
-    private _date: Date;
-    private _amount: number;
-    private _value: number;
 
-    constructor(date: Date, amount: number, value: number) {
-        this._date = date;
-        this._amount = amount;
-        this._value = value;
+    // Example 1
+
+    // private _date: Date;
+    // private _amount: number;
+    // private _value: number;
+
+    // constructor( date: Date, amount: number, value: number) {
+    //     this._date = date;
+    //     this._amount = amount;
+    //     this._value = value;
+    // }
+
+    // ---------------------------------------------------
+
+    // Example 2
+
+    // constructor( 
+    //     private _date: Date,
+    //     private _amount: number,
+    //     private _value: number
+    // ) {}
+
+    // get date(): Date {
+    //     return this._date;
+    // }
+
+    // get amount(): number {
+    //     return this._amount;
+    // }
+
+    // get value(): number {
+    //     return this._value;
+    // }
+
+    // ---------------------------------------------------
+
+    constructor( 
+        private _date: Date,
+        public readonly amount: number,
+        public readonly value: number
+    ) {}
+
+    get volume(): number {
+        return this.amount * this.value;
     }
 
     get date(): Date {
-        return this._date;
-    }
-
-    get amount(): number {
-        return this._amount;
-    }
-
-    get value(): number {
-        return this._value;
-    }
-
-    get volume() {
-        return this._amount * this._value;
+        const date = new Date(this._date.getTime());
+        return date;
     }
 }
--- app/models/negotiations.ts
+++ app/models/negotiations.ts
@@ -1,13 +1,16 @@
 import { Negotiation } from "./negotiation.js";
 
 export class Negotiations {
-  private negotiations: Array<Negotiation> = [];
+  
+  // private negotiations: Array<Negotiation> = [];
+  private negotiations: Negotiation[] = [];
 
   add(negotiation: Negotiation) {
     this.negotiations.push(negotiation);
   }
 
-  list(): ReadonlyArray<Negotiation> {
+  // list(): ReadonlyArray<Negotiation> {
+  list(): readonly Negotiation[] {
     return this.negotiations;
   }
 }
